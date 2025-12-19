f790f73312dd6b00a3ce0cb5cbfaf784788ecfe6
Arman Baadpa <138382878+Alendra405@users.noreply.github.com>
2025-12-19 14:10:51 +0330

    Update X.py

--- X.py
+++ X.py
@@ -1,11 +1,14 @@
-from qiskit import QuantumCircuit
-from qiskit.primitives import StatevectorSampler
+from qiskit import QuantumCircuit, transpile
+from qiskit_aer import AerSimulator
 
-qc = QuantumCircuit(1)
-qc.x(0)
-qc.measure_all()
-sampler = StatevectorSampler()
-result = sampler.run([qc], shots=1024).result()
-print(result[0].data.meas.get_counts())
-
-
+qc = QuantumCircuit(1,1)
+while True:
+    inp = int(input("For turn on qubit type '1' esle '0' : "))
+    qc.x(0)
+    qc.measure(0,0)
+    backend = AerSimulator()
+    transpiled = transpile(qc, backend)
+    job = backend.run(transpiled, shots=1)
+    result = job.result()
+    counts = result.get_counts()
+    print(counts)
